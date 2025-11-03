e24f39e78c1ad0144d52afc6eef65383b54bb7de
Ayenew Girmay <ayenewgirmay21@gmail.com>
2025-11-02 16:53:14 -0800

    Add backend files

---
+++ backend/app.js
@@ -0,0 +1,43 @@
+
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import cookieParser from "cookie-parser";
+import {config} from "dotenv"
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+const app = express();
+config({path: "./config/config.env" });
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL_ONE || "http://localhost:5173",
+      process.env.FRONTEND_URL_TWO || "http://localhost:5174"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
